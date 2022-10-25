91a056284fbe00271fab25a7eeb2dd2852a21588
Yuri Mednikov <mednikov.iurii@gmail.com>
2022-10-25 17:04:42 +0200

    Added properties files

--- src/main/java/xyz/mednikov/examples/fluxnote/model/Note.java
+++ src/main/java/xyz/mednikov/examples/fluxnote/model/Note.java
@@ -1,5 +1,9 @@
 package xyz.mednikov.examples.fluxnote.model;
 
-public record Note(String id, String content, String userId) {
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "notes")
+public record Note(@Id String id, String content, String userId) {
     
 }
